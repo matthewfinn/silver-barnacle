cc7f557c9d1996286779386c1f8bcac4bfd0aba4
matthewfinn <matt.finn@hotmail.com>
2024-10-30 19:28:57 +0000

    changes to printing

--- js/annotations/readAndDraw.js
+++ js/annotations/readAndDraw.js
@@ -9,8 +9,9 @@ async function readAndDraw(fileURL) {
       const $ = cheerio.load(html)
       const rows = [];
 
-      const selector = 'tbody tr'
+      const selector = 'tbody tr' // selector for table on webpage
 
+      // get the elements from the webpage 
       $(selector).each(function (i, e) {
         const row = [];
         rows.push(row);
@@ -36,32 +37,57 @@ async function readAndDraw(fileURL) {
 
       const asplitIndex = 1 // it's the item at index 1 in each group that we would like to print 
       // Print only the 2nd element of each sub-array, grouped by key
-
-      // const map = new Map(Object.entries(yValues));
-      // const sortedMap = new Map([...map.entries()].sort((a, b) => Number(b[0]) - Number(a[0])));
-      // const sortedObj = Object.fromEntries(sortedMap);
-
-
-      // console.log(sortedObj);
-      // console.log(Object.keys(sortedObj)); // Should print: [ '2', '1', '0' ]
-      // console.log(sortedMap); // This will display the sorted order clearly
-
-
-
       // Step 1: Get the keys of the object
       const keys = Object.keys(yValues);
 
       // Step 2: Sort the keys in descending order
       keys.sort((a, b) => Number(b) - Number(a));
 
+      console.log("KEYS:" + keys)
 
-      for (let i = keys.length - 1; i >= 0; i--) {
-        //console.log(`Group ${key}:`);
-        const key = keys[i];
-        yValues[key].forEach(item => {
+      for (const element of keys) {
+
+        //console.log("XVals for " + element +": " + yValues[element])
+
+        var xValues = yValues[element];
+        xValues.sort((a, b) => a[0] - b[0]);
+
+        //console.log("isitanarray???? " + (xValues.constructor == Array)); // true
+
+        //console.log("XVals for " + element +": " + xValues)
+
+        let lastValue = null
+
+        yValues[element].forEach(item => {
+
+          const currentValue = item[0]; // x - coordinate
+
+          if (lastValue == null) {
+            for (let index = 0; index < currentValue; index++) {
+              process.stdout.write(' '); // Print the 2nd element of each sub-array
+            }
+
+          } else {
+            if (currentValue === lastValue) {
+              //process.stdout.write('\u0020'); 
+            } else {
+              const spaces = currentValue - lastValue - 1;
+              //console.log("GAP between " + currentValue + " and " + lastValue + " is " + spaces); // separation between groups
+              if (spaces > 1) {
+                for (let index = 0; index < spaces; index++) {
+                  process.stdout.write('\u0020');
+                }
+              }
+              //console.log(`Current value (${currentValue}) is different from the last value.`);
+            }
+
+          }
+
           process.stdout.write(item[asplitIndex]); // Print the 2nd element of each sub-array
+          lastValue = currentValue; // Update lastValue for the next iteration
+
         });
-        console.log(); // separation between groups
+        console.log(); // separation between groups of the y axis
       }
 
 
@@ -72,4 +98,5 @@ async function readAndDraw(fileURL) {
 
 };
 
-readAndDraw("https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub")
+//readAndDraw("https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub")
+readAndDraw("https://docs.google.com/document/d/e/2PACX-1vShuWova56o7XS1S3LwEIzkYJA8pBQENja01DNnVDorDVXbWakDT4NioAScvP1OCX6eeKSqRyzUW_qJ/pub");
