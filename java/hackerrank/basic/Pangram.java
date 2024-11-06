f671c6e6b127d5e41499b718ed91136405c6afa4
matthewfinn <matt.finn@hotmail.com>
2024-11-06 17:14:54 +0000

    Reorg

---
+++ java/hackerrank/basic/Pangram.java
@@ -0,0 +1,41 @@
+public class Pangram {
+
+    public static void main (String[] args){
+
+        String s = "We promptly judged antique ivory buckles for the next prize";
+        System.out.println(pangrams(s));
+
+    }
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String pangrams(String s) {
+        s =  s.toLowerCase();
+        int index = 0;
+        boolean[] mark = new boolean[26];
+
+        for(char character :  s.toCharArray()){
+            System.out.printf("The value of character %s is %d%n", character, Character.getNumericValue(character));
+            // characters have a numeric value from 10 - 35. i.e. a=10, z=35
+            if ('a' <= character
+                    && character <= 'z')
+
+                index = character - 'a';
+
+                // If this character is other than english
+                // lowercase and uppercase characters.
+            else
+                continue;
+            mark[index] = true;
+        }
+
+        for(boolean b: mark) if(b==false) return "not pangram";
+        return "pangram";
+
+    }
+}
---
+++ java/hackerrank/basic/PermutingTwoArrays.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class PermutingTwoArrays {
+
+    public static void main (String[] args){
+
+    }
+
+    /*
+     * Complete the 'twoArrays' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY A
+     *  3. INTEGER_ARRAY B
+     */
+
+    public static String twoArrays(int k, List<Integer> A, List<Integer> B) {
+        // Write your code here
+
+        return "";
+
+    }
+}
